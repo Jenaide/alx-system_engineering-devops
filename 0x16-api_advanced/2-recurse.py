100bc33f8532b6df37c419a7993098a9d4a76bb5
Jenaide <jenaidesibolie@gmail.com>
2023-06-06 19:21:50 +0000

    added task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,3 +2,27 @@
 """
 Created by Jenaide Sibolie
 """
+from json import loads
+from requests import get
+
+
+def recurse(subreddit, hot_list=[]):
+    """
+    A recursive function that queries the reddit api and returns a list containing
+    the title of all hot articles for given subreddit.
+    if no results return None.
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    # Set a custom User-Agent header
+    headers = {'User-Agent': 'Windows; 11 Home Single Language; 22621.1778; Chrome/113.0.5672.129'}
+    response = get(url, headers=headers, allow_redirects=False)
+    reddits = response.json()
+
+    try:
+        children = reddits.get('data').get('children')
+        for title in children:
+            hot_list.append(title.get('data').get('title'))
+        return hot_list
+    except:
+        print(None)
+        return 0
