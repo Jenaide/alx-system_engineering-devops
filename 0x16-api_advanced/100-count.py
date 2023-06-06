13fe2afee03f9a229130633f35608cc2e9817211
Jenaide <jenaidesibolie@gmail.com>
2023-06-06 19:57:53 +0000

    added the additional task 100

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Created by Jenaide Sibolie
+"""
+from json import loads
+from requests import get
+
+
+def count_words(subreddit, word_list, after=None):
+    """
+    A Recursive function that queries the reddit api, parses the title of all hot articles
+    and prints a sorted count of given keywords.
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    # Set a custom User-Agent header
+    headers = {'User-Agent': 'Windows; 11 Home Single Language; 22621.1778; Chrome/113.0.5672.129'}
+    params = {'after': after} if after else None
+    response = get(url, headers=headers, params=params, allow_redirects=False)
+    reddits = response.json()
+
+    try:
+        children = reddits.get('data').get('children')
+        for child in children:
+            title = child.get('data').get('title')
+            for word in word_list:
+                if word.lower() in title.lower().split():
+                    if word.lower() in word_count:
+                        word_count[word.lower()] += 1
+                    else:
+                        word_count[word.lower()] = 1
+
+        after = reddits.get('data').get('after')
+        if after:
+            count_words(subreddit, word_list, after=after)
+        else:
+            print_word_count(word_count)
+    except:
+        print(None)
+
+def print_word_count(word_count):
+    sorted_words = sorted(word_count.items(), key=lambda x: (-x[1], x[0].lower()))
+    for word, count in sorted_words:
+        print("lowercase word:".format(word.lower(), count))
