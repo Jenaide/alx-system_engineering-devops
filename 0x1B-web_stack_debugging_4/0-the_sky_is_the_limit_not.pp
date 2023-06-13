d6b573a7b5b73af666ca559ae8fc455f0a1aea35
root <jenaidesibolie@gmial.com>
2023-06-13 18:46:42 +0000

    added task 2

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# fixing the NGINX requests limit
+exec { 'upgrade':
+  path    => '/bin/',
+  command => 'sed -i "s/15/4096" /etc/default/nginx',
+}
+
+exec { 'restart':
+  path    => '/usr/bin',
+  command => 'service nginx restart',
+}
